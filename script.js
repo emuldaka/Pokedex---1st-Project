63601062ca5d698e748675cd7690a6525f8507d1
Elis <emuldaka@gmail.com>
2023-03-14 21:21:37 -0500

    added read me summary on project plans/functionality

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-console.log("JavaScript is ready to run");
-
 const fetchPokemon = () => {
   const promises = [];
   for (let i = 1; i <= 150; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then((res) => res.json()));
+    if (i == 1) {
+    }
+    console.log(promises);
   }
 };
+
+Promise.all(promises).then((results) => {
+  const pokemon = results.map((result) => ({
+    name: result.name,
+    image: result.sprites["front_default"],
+    type: result.types.map((type) => type.type.name).join(", "),
+    id: result.id,
+  }));
+  displayPokemon(pokemon);
+});
