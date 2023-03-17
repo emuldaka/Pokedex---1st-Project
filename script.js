5068d079e68849a8569031c43e322f7ea0243ed0
Elis <emuldaka@gmail.com>
2023-03-17 15:02:39 -0500

    added the fucntion for display pokemon that will map the pokemon's image, name and tpye to the html

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ Promise.all(promises).then((results) => {
   }));
   displayPokemon(pokemon);
 });
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokeman) => `
+      <li class="card">
+          <img class="card-image" src="${pokeman.image}"/>
+          <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+          <p class="card-subtitle">Type: ${pokeman.type}</p>
+      </li>
+  `
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
